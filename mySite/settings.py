1b73f4ee102ae1d5fa20edc53c8a36b857058c6b
seyoner <usupovismail613@gmail.com>
2024-10-06 10:48:20 +0300

    update setthings

--- mySite/settings.py
+++ mySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yjw(q50!lt!#0!52g9z@8%u^52ep6)gr@)(1ynv+kh3hrbng+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "pythonanywhere.com"]
 
 
 # Application definition
